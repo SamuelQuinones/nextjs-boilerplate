4b39051fbcf4f0e036d1ac18ed23f19eb0f277f2
Samuel Quiñones <sdquinones1@gmail.com>
2022-04-19 22:05:13 -0400

    added NextSEO

---
+++ src/pages/features.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import { NextSeo } from "next-seo";
+
+const Features: NextPage = () => {
+  return (
+    <>
+      <NextSeo title="Features" />
+      <h1 style={{ textAlign: "center" }}>Coming soon...</h1>
+    </>
+  );
+};
+
+export default Features;
